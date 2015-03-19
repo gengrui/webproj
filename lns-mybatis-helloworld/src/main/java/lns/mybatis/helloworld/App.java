ec241865232f50e5e68b92666a13c34f44287495
gr <contactgr@126.com>
2015-03-19 14:48:48 +0800

    首次提交

---
+++ lns-mybatis-helloworld/src/main/java/lns/mybatis/helloworld/App.java
@@ -0,0 +1,13 @@
+package lns.mybatis.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
